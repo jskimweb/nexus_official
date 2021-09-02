4efeb02a6108c162b1d207f6f0883605864d643c
jskimweb <jskimweb@gmail.com>
2021-09-02 17:53:07 +0900

    Complete PC Ver.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.onload = function(){
+	var bullets = $('.sw-visual__pg .swiper-pagination-bullet');
+
     $(window).scroll(function(){  
         var scY = $(window).scrollTop();
 
@@ -8,11 +10,10 @@ window.onload = function(){
 		else {
             $('.header').removeClass('header--scrolled');
         }
-
     });
-  
-  	var sw_visual = new Swiper('.sw-visual', {
-    	loop: true,
+	
+	var sw_visual = new Swiper('.sw-visual', {
+		loop: true,
     	effect: 'fade',
     	crossEffect: {
       		crossFade: true,
@@ -20,11 +21,35 @@ window.onload = function(){
     	autoplay: {
      	delay: 3000,
      	disableOnInteraction: false,
-    	},
-    	speed: 1200,
+	},
+	speed: 1200,
 		pagination: {
 			el: '.sw-visual__pg',
 			clickable: true,
 		},
-  	})
+		on: {
+			slideChange: function() {
+				changePg(this.realIndex);
+			}
+		},
+	})
+	
+	
+	var bulletsIndex = 0;
+
+	function changePg(_num) {
+		if (_num == bulletsIndex) {
+			return;
+		}
+        $('.sw-visual__pg .swiper-pagination-bullet').removeClass('sw-visual__pg--active');
+        $('.sw-visual__pg .swiper-pagination-bullet').eq(bulletsIndex).addClass('sw-visual__pg--active');
+        bulletsIndex = _num;
+    }
+
+
+    $.each(bullets, function(index, item) {
+		$(this).click(function() {
+			changePg(index);
+		});
+	});
 };
